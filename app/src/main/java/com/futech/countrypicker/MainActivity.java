bd56ded0127f37c12352bcb84c80d2488e636898
santosh102 <santoshmadani102@gmail.com>
2021-12-18 14:37:49 -0800

    initial commit

---
+++ app/src/main/java/com/futech/countrypicker/MainActivity.java
@@ -0,0 +1,30 @@
+package com.futech.countrypicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rilixtech.widget.countrycodepicker.Country;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    CountryCodePicker ccp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected(Country selectedCountry) {
+                Toast.makeText(getApplicationContext(), "Updated " + selectedCountry.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
